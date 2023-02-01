526cacbf9ec762aaf48b3a949f067a0092d6fb39
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-02-01 16:22:16 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+const taskList = document.querySelector("#tasks");
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const li = document.createElement("li");
+    li.textContent = e.target.newTaskDescription.value;
+
+    const btn = document.createElement("button");
+    btn.textContent = "X";
+    li.append(btn);
+
+    btn.addEventListener("click", () => {
+      li.remove();
+    });
+
+    taskList.append(li);
+    form.reset();
+  });
 });
